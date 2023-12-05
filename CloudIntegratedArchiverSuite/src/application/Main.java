f97b1f917063fd63eb6ca9b9b9a9f3c673fc4047
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-12-05 13:11:08 +0000

    added base main

---
+++ CloudIntegratedArchiverSuite/src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
