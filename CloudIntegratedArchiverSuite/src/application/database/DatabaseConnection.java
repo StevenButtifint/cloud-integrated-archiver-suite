7c1de04fc9d0030fb55650254a60d56201d260a8
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-02-08 14:52:41 +0000

    added create link table method

--- CloudIntegratedArchiverSuite/src/application/database/DatabaseConnection.java
+++ CloudIntegratedArchiverSuite/src/application/database/DatabaseConnection.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnection {
+	
+	private static String LINK_TABLE_NAME  = "links";
 
     public Connection connectToDatabase(String databaseName, String username, String password) {
     	Connection connection=null;
@@ -22,4 +24,17 @@ public class DatabaseConnection {
     	}
     	return connection;
     }
-}
+    public void createLinkTable(Connection connection) { 
+    	Statement statement;
+    	try {
+    		String query = "CREATE TABLE " + LINK_TABLE_NAME + "(empid SERIAL, name TEXT, description TEXT, source_location TEXT, destination_location TEXT, created_date DATE, last_synced DATE, accessible_state BOOLEAN, primary key(empid));";
+    		statement = connection.createStatement();
+    		statement.executeUpdate(query);
+    		System.out.println("table created");
+    	} catch (Exception e) {
+    		System.out.println(e);
+    	}
+    }
+    
+    public void insertLink(Connection connection, String name, String description, String source_location, String destination_location) {
+    
