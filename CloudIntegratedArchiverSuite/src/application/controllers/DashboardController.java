bfd0fc9e690b5258f672d14e1399ffb6358674da
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-02-13 16:23:40 +0000

    updated attributes

--- CloudIntegratedArchiverSuite/src/application/controllers/DashboardController.java
+++ CloudIntegratedArchiverSuite/src/application/controllers/DashboardController.java
@@ -1,13 +1,27 @@
 package application.controllers;
 
+import java.io.IOException;
+import java.util.List;
+
 import application.models.Dashboard;
-import application.services.DashboardService;
+import application.models.Link;
+import application.threads.AvailableLinksThread;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
 
 
 public class DashboardController extends BaseController {
-	Dashboard dashboard = new Dashboard();
-	private DashboardService dashboardService;
+	
+	private static final String LINK_ITEM_PATH = "../views/link_item.fxml";
 
+	@FXML
+	private VBox dashboardLinkList;// = null;
+	
+	AvailableLinksThread availableLinksThread;
+	Dashboard dashboard = new Dashboard();
 	
     public void initialize() {
     }
