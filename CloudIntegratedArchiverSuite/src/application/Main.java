66622f1cad7b0635aaae13b0653aa674a04c3a40
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-02-26 13:43:36 +0000

    added index view and controller loading directly in main

--- CloudIntegratedArchiverSuite/src/application/Main.java
+++ CloudIntegratedArchiverSuite/src/application/Main.java
@@ -1,19 +1,48 @@
 package application;
-	
-import application.controllers.IndexController;
-import application.util.ViewNavigator;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
+import java.io.IOException;
+
+import application.config.Config;
+import application.controllers.IndexController;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
-	
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// load initial view
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(Config.getProperty("index.view.path")));
+			Parent root = loader.load();
+
+			// get controller for the view
+			IndexController controller = loader.getController();
+
+			// set the primary stage
+			primaryStage.setTitle(Config.getProperty("application.title"));
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.setScene(new Scene(root, 1000, 600));
+			primaryStage.show();
+
+		} catch (IOException e) {
+			// display an error message to the user
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setTitle("Failed to launch Archiver Suite");
+			alert.setHeaderText("Application Error");
+			alert.setContentText("An error occurred while loading the application.");
+			alert.showAndWait();
+			Platform.exit();
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-    @Override
-    public void start(Stage primaryStage) {
-    	ViewNavigator.loadView("../views/index.fxml", new IndexController(), "Archiver Suite");
-    }
 }
