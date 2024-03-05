110157959a655b57e7939752637b7a57df841f20
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-03-05 11:14:14 +0000

    added attributes

--- CloudIntegratedArchiverSuite/src/application/controllers/LinkItemController.java
+++ CloudIntegratedArchiverSuite/src/application/controllers/LinkItemController.java
@@ -4,17 +4,37 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class LinkItemController {
+
+	private SyncLinkThread syncLinkThread;
+	private AvailableLinkThread availableLinkThread;
+
+	private Link link;
+	@FXML
+	private Label linkName;
+
+	@FXML
+	private Label linkDescription;
+
+	@FXML
+	private Label availableStatus;
+
+	@FXML
+	private Label linkNotice;
 	
 	@FXML
-	Label linkName;
-	
+	private Button syncButton;
+
 	@FXML
-	Label linkDescription;
+	private AnchorPane backgroundPane;
+
+	private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
+
+
 	
 	public void setLinkName(String name) {
 		linkName.setText(name);
 	}
-	
+
 	public void setLinkDescription(String description) {
 		linkDescription.setText(description);
 	}
