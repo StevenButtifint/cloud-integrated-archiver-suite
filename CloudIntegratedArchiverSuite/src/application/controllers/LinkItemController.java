cbe943ad6a733e1402784a0a79a6b28ee028f310
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-03-06 21:42:11 +0000

    added accessible state methods

--- CloudIntegratedArchiverSuite/src/application/controllers/LinkItemController.java
+++ CloudIntegratedArchiverSuite/src/application/controllers/LinkItemController.java
@@ -30,7 +30,28 @@ public class LinkItemController {
 	private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
 
 
-	
+	public void setStateAccessible() {
+		syncButton.setDisable(false);
+		availableStatus.setText("AVAILABLE");
+		availableStatus.getStyleClass().setAll("link-available-label");
+		backgroundPane.getStyleClass().remove("link-item-inaccessible");
+		backgroundPane.getStyleClass().add("link-item-accessible");
+		linkName.getStyleClass().setAll("link-available-text");
+		linkDescription.getStyleClass().setAll("link-available-text");
+		syncedLabel.getStyleClass().setAll("link-available-text");
+	}
+
+	public void setStateInaccessible() {
+		syncButton.setDisable(true);
+		availableStatus.setText("UNAVAILABLE");
+		availableStatus.getStyleClass().setAll("link-unavailable-label");
+		backgroundPane.getStyleClass().remove("link-item-accessible");
+		backgroundPane.getStyleClass().add("link-item-inaccessible");
+		linkName.getStyleClass().setAll("link-unavailable-text");
+		linkDescription.getStyleClass().setAll("link-unavailable-text");
+		syncedLabel.getStyleClass().setAll("link-unavailable-text");
+	}
+
 	public void setLinkName(String name) {
 		linkName.setText(name);
 	}
@@ -38,5 +59,4 @@ public class LinkItemController {
 	public void setLinkDescription(String description) {
 		linkDescription.setText(description);
 	}
-	
 }
