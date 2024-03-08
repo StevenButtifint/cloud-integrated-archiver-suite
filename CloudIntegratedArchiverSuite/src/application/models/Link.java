c049898a7d2a4e2577e29cbb70281e79bb87e555
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-03-08 14:02:22 +0000

    grouped methods

--- CloudIntegratedArchiverSuite/src/application/models/Link.java
+++ CloudIntegratedArchiverSuite/src/application/models/Link.java
@@ -1,10 +1,10 @@
 package application.models;
 
 public class Link {
-	
+
 	int id;
 	String name;
-	String description;	
+	String description;
 	String source;
 	String destination;
 	String createdDate;
@@ -23,59 +23,60 @@ public class Link {
 		this.lastSynced = lastSynced;
 		this.accessible = accessible;
 	}
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public String getSource() {
 		return source;
-	}
-
-	public void setSource(String source) {
-		this.source = source;
 	}
 
 	public String getDestination() {
 		return destination;
 	}
 
-	public void setDestination(String destination) {
-		this.destination = destination;
 	}
 
 	public String getLastSynced() {
 		return lastSynced;
 	}
 
-	public void setLastSynced(String lastSynced) {
-		this.lastSynced = lastSynced;
-	}
-
-	public boolean isAccessible() {
+	public boolean getAccessible() {
 		return accessible;
 	}
 
-	public void setAccessible(boolean accessible) {
-		this.accessible = accessible;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getCreatedDate() {
 		return createdDate;
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public void setLastSynced(Date lastSynced) {
+		this.lastSynced = lastSynced;
+	}
+
+	public void setAccessible(boolean accessible) {
+		this.accessible = accessible;
 	}
 }
