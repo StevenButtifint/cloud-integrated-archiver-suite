d83b71d436aad82b8ccb1fda36057cf1ef9b10b1
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-05-01 15:45:40 +0100

    added setup methods to start

--- CloudIntegratedArchiverSuite/src/application/Main.java
+++ CloudIntegratedArchiverSuite/src/application/Main.java
@@ -14,48 +14,84 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import application.config.Config;
-import application.controllers.IndexController;
+import application.database.DatabaseConnection;
 import application.util.AlertError;
 
 public class Main extends Application {
 	private static final Logger logger = LogManager.getLogger(Main.class.getName());
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			// load application properties
-			Config config = new Config("app.properties");
-			logger.info("Successfully loaded application properties.");
+	private static final String APP_PROPERTIES = "app.properties";
+	private static final String VIEW_PATH_INDEX = "view.path.index";
+	private static final String APP_TITLE = "app.title";
 
-			// load index view
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(config.getProperty("view.path.index")));
-			Parent root = loader.load();
-			logger.info("Successfully loaded index view.");
-
-			// get controller for the view
-			IndexController controller = loader.getController();
-
-			// set the primary stage
-			primaryStage.setTitle(config.getProperty("app.title"));
-			primaryStage.initStyle(StageStyle.UNDECORATED);
-			primaryStage.setScene(new Scene(root, 1000, 600));
-			primaryStage.show();
-			logger.info("Successfully setup primary stage.");
-
-		} catch (IOException e) {
-			logger.error("Failed to setup the primary stage: " + e.getMessage(), e);
-
-			// display an error message to the user
-			AlertError.showError("Failed to launch Archiver Suite", "Application Error",
-					"An error occurred while loading the application.");
-
-			// exit application
-			Platform.exit();
-		}
-	}
+	private DatabaseConnection databaseConnection;
+	private Config config;
 
 	public static void main(String[] args) {
 		logger.info("Starting application...");
 		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		if (!initialiseConfiguration()) {
+			return;
+		}
+
+		if (!initialiseDatabaseTables()) {
+			return;
+		}
+
+		if (!setupPrimaryStage(primaryStage)) {
+			Platform.exit();
+		}
+	}
+
+	private boolean initialiseConfiguration() {
+		try {
+			config = new Config(APP_PROPERTIES);
+			logger.info("Successfully loaded application properties.");
+			return true;
+		} catch (Exception e) {
+			logger.error("Failed to load application properties: " + e.getMessage(), e);
+			AlertError.showError("Failed to launch Archiver Suite", "Application Error",
+					"Cannot load application properties.");
+			return false;
+		}
+	}
+
+	private boolean initialiseDatabaseTables() {
+		databaseConnection = new DatabaseConnection();
+		if (!databaseConnection.connectToDatabase()) {
+			logger.error("Unable to connect to Link database.");
+			AlertError.showError("Database Failure", "Connection Error", "Cannot connect to the database.");
+			return false;
+		}
+		if (!databaseConnection.initialiseLinkTable()) {
+			logger.error("Unable to initialise Link database.");
+			databaseConnection.closeConnection();
+			AlertError.showError("Database Failure", "Initialisation Error", "Cannot initialise the database table.");
+			return false;
+		}
+		databaseConnection.closeConnection();
+		return true;
+	}
+
+	private boolean setupPrimaryStage(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(config.getProperty(VIEW_PATH_INDEX)));
+			Parent root = loader.load();
+			primaryStage.setTitle(config.getProperty(APP_TITLE));
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.setScene(new Scene(root, 1000, 600));
+			primaryStage.show();
+			logger.info("Successfully setup primary stage.");
+			return true;
+		} catch (IOException e) {
+			logger.error("Failed to setup the primary stage: " + e.getMessage(), e);
+			AlertError.showError("Failed to launch Archiver Suite", "UI Loading Error", "Failed to load user interface.");
+			return false;
+		}
+	}
+
 }
