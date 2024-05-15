7c3f3f020e61db1a4255de35d5594350ed8feed0
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-05-15 13:23:40 +0100

    added list all files method

--- CloudIntegratedArchiverSuite/src/application/util/FileOperations.java
+++ CloudIntegratedArchiverSuite/src/application/util/FileOperations.java
@@ -1,12 +1,31 @@
 package application.util;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileOperations {
-	
-    public static boolean validDirectory(String directoryPath) {
-        File directory = new File(directoryPath);
-        return directory.exists() && directory.isDirectory();
-    }
+
+	public static boolean validDirectory(String directoryPath) {
+		File directory = new File(directoryPath);
+		return directory.exists() && directory.isDirectory();
+	}
+
+	public static List<File> listAllFiles(String folderPath) {
+		List<File> fileList = new ArrayList<>();
+		File folder = new File(folderPath);
+		File[] files = folder.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				if (file.isDirectory()) {
+					fileList.addAll(listAllFiles(file.getPath()));
+				} else {
+					fileList.add(file);
+				}
+			}
+		}
+		return fileList;
+	}
+
 
 }
