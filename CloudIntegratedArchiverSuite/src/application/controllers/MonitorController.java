79b92892b6051a5a134c633d0d4bc33f15a8e5c9
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-05-16 11:17:29 +0100

    added monitor controller

--- CloudIntegratedArchiverSuite/src/application/controllers/MonitorController.java
+++ CloudIntegratedArchiverSuite/src/application/controllers/MonitorController.java
@@ -1,7 +1,31 @@
 package application.controllers;
 
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import application.config.Config;
+import application.models.LinkOperationDetails;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
 public class MonitorController {
-	
-    public void initialize() {
-    }
+
+	private static final Logger logger = LogManager.getLogger(MonitorController.class.getName());
+
+	private Config appConfig;
+
+	@FXML
+	private VBox operationsVBox;
+
+	public MonitorController(Config appConfig) {
+		this.appConfig = appConfig;
+	}
+
+	public void initialize() {
+	}
+
 }
