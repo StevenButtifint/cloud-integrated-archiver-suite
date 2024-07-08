adb2cf644c5c5afea42494f921b3669424401b1f
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2024-07-08 20:17:07 +0100

    added save operations log method

--- CloudIntegratedArchiverSuite/src/application/util/LinkOperationManager.java
+++ CloudIntegratedArchiverSuite/src/application/util/LinkOperationManager.java
@@ -1,12 +1,22 @@
 package application.util;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import application.interfaces.OperationManager;
 import application.models.LinkOperationDetails;
 
 public class LinkOperationManager implements OperationManager {
+
+	private static final Logger logger = LogManager.getLogger(LinkOperationManager.class.getName());
 
 	private List<LinkOperationDetails> linkOperations;
 
@@ -22,4 +32,19 @@ public class LinkOperationManager implements OperationManager {
 		linkOperations.clear();
 	}
 
+	public boolean saveOperations() {
+		String timePostfix = LocalTime.now().format(DateTimeFormatter.ofPattern("HH-mm-ss"));
+		String fileName = "operations-save_" + timePostfix + ".txt";
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+			for (LinkOperationDetails linkOperation : linkOperations) {
+				writer.write(linkOperation.getDetailsFormatted());
+				writer.newLine();
+			}
+			return true;
+		} catch (IOException e) {
+			logger.error("Unable to save operations file.", e);
+		}
+		return false;
+	}
 }
